a9cbae75c939e1e2cdde74c931c3b5ab0fbd70e1
j001942025 <jaelson.araujo@linksearch.com.br>
2019-04-09 18:30:42 -0300

    atualização

--- platforms/browser/www/js/funcoes.js
+++ platforms/browser/www/js/funcoes.js
@@ -1,110 +1,137 @@
 var total = 0;
 var atual = 0;
 var operador = '+';
+var resultado = false;
 
 function adicionar(valor){
-	atual = atual * 10;
-	atual = atual + valor;
-	document.getElementById("visor").value = atual;
+	
+	if(resultado == true && atual == 0){
+		total = 0;
+		atual = valor;
+		document.getElementById("visor").value = atual;
+		resultado = false;
+	}else{
+		atual = atual * 10;
+		atual = atual + valor;
+		document.getElementById("visor").value += valor;
+	}
+	
 }
 
 function somar(){
-	switch(operador){
-		case '+':
-			total = total + atual;
-			break;
-		case '-':
-			total = total - atual;
-			break;
-		case '*':
-			total = total * atual;
-			break;
-		case '/':
-			total = total / atual;
-			break;
-	}
-	document.getElementById("visor").value = '';
-	atual = 0;
+		switch(operador){
+			case '+':
+				total = total + atual;
+				break;
+			case '-':
+				total = total - atual;
+				break;
+			case '*':
+				total = total * atual;
+				break;
+			case '/':
+				total = total / atual;
+				break;
+		}
+		document.getElementById("visor").value = document.getElementById("visor").value + " + ";
+		atual = 0;
+		if(resultado == true && atual == 0){
+			resultado = false;
+		}
 }
 
 function subtrair(){
-	switch(operador){
-		case '+':
-			total = total + atual;
-			break;
-		case '-':
-			total = total - atual;
-			break;
-		case '*':
-			total = total * atual;
-			break;
-		case '/':
-			total = total / atual;
-			break;
-	}
-	document.getElementById("visor").value = '';
-	atual = 0;
-	operador = '-';
+		switch(operador){
+			case '+':
+				total = total + atual;
+				break;
+			case '-':
+				total = total - atual;
+				break;
+			case '*':
+				total = total * atual;
+				break;
+			case '/':
+				total = total / atual;
+				break;
+		}
+		document.getElementById("visor").value = document.getElementById("visor").value + " - ";
+		atual = 0;
+		operador = '-';
+	if(resultado == true && atual == 0){
+			resultado = false;
+		}
 }
 
 function multiplicar(){
-	switch(operador){
-		case '+':
-			total = total + atual;
-			break;
-		case '-':
-			total = total - atual;
-			break;
-		case '*':
-			total = total * atual;
-			break;
-		case '/':
-			total = total / atual;
-			break;
-	}
-	document.getElementById("visor").value = '';
-	atual = 0;
-	operador = '*';
+		switch(operador){
+			case '+':
+				total = total + atual;
+				break;
+			case '-':
+				total = total - atual;
+				break;
+			case '*':
+				total = total * atual;
+				break;
+			case '/':
+				total = total / atual;
+				break;
+		}
+		document.getElementById("visor").value = document.getElementById("visor").value + " * ";
+		atual = 0;
+		operador = '*';
+	if(resultado == true && atual == 0){
+			resultado = false;
+		}
 }
 
 function dividir(){
-	switch(operador){
-		case '+':
-			total = total + atual;
-			break;
-		case '-':
-			total = total - atual;
-			break;
-		case '*':
-			total = total * atual;
-			break;
-		case '/':
-			total = total / atual;
-			break;
-	}
-	document.getElementById("visor").value = '';
-	atual = 0;
-	operador = '/';
+		switch(operador){
+			case '+':
+				total = total + atual;
+				break;
+			case '-':
+				total = total - atual;
+				break;
+			case '*':
+				total = total * atual;
+				break;
+			case '/':
+				total = total / atual;
+				break;
+		}
+		document.getElementById("visor").value = document.getElementById("visor").value + " / ";
+		atual = 0;
+		operador = '/';
+	if(resultado == true && atual == 0){
+			resultado = false;
+		}
 }
 
 function igual(){
-	switch(operador){
-		case '+':
-			total = total + atual;
-			break;
-		case '-':
-			total = total - atual;
-			break;
-		case '*':
-			total = total * atual;
-			break;
-		case '/':
-			total = total / atual;
-			break;
+	if(resultado == false){
+		switch(operador){
+			case '+':
+				total = total + atual;
+				break;
+			case '-':
+				total = total - atual;
+				break;
+			case '*':
+				total = total * atual;
+				break;
+			case '/':
+				total = total / atual;
+				break;
+		}
+		var x = document.getElementById("visor").value;
+		x = x.replace(/\/=/g, '/');
+		document.getElementById("visor").value = x + '\n =' + total;
+		atual = 0;
+		operador = '+';
+		resultado = true;
 	}
-	document.getElementById("visor").value = total;
-	atual = 0;
-	operador = '+';
 }
 
 function limpar(){
@@ -112,4 +139,5 @@ function limpar(){
 	total = 0;
 	operador = '+';
 	document.getElementById("visor").value = '';
+	resultado = false;
 }
